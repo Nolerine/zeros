fbbe6c96b2e68568d023cb7095af50fb9441ac36
Nolerine <nolerine@mail.ru>
2019-09-26 18:02:52 +0300

    feat: implement zeros counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let zer = 0;
+  let work = 1n;
+  let multipliers = expression.split("*");      // разбиение строки в массив факториалов
+  
+  for (let i=0; i < multipliers.length; i++) {
+    if (multipliers[i].endsWith('!!')) {
+      multipliers[i] = doubleFact(BigInt(multipliers[i].slice(0, multipliers[i].length-2)));
+    } else {
+      multipliers[i] = fact(BigInt(multipliers[i].slice(0, multipliers[i].length-1)));
+    }
+  }
+
+  for (let i=0; i < multipliers.length; i++) {    // перемножение факториалов
+    work *= multipliers[i];
+  }
+
+  while (work % 10n == 0n) {    //подсчет нулей
+    work /= 10n;
+    zer++;
+  }
+
+  return zer;
+}
+
+function fact(a) {
+if (a === 0n) {
+  return 1n;
+} else {
+    return a * fact(a-1n);
+  }
+}
+
+function doubleFact(a) {
+  if (a <= 0n) {
+    return 1n;
+  } else {
+      return a * doubleFact(a-2n);
+  }
 }
